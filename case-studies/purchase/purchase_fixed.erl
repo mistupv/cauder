f0636ab2a25134f32a0f94e04d954dd75dfffbdc
Juan José González Abril <juanjo@gonzabril.es>
2020-07-28 01:25:24 +0200

    Format case studies code

--- case-studies/purchase/purchase_fixed.erl
+++ case-studies/purchase/purchase_fixed.erl
@@ -1,34 +1,37 @@
 %FASE 2014 example
 
 -module(purchase_fixed).
--export([main/0,asynchAnd/2,checkCredit/2,checkAddress/1,checkItem/1]).
- 
-main() -> Pid=spawn(?MODULE,asynchAnd,[3,self()]),
-	  spawn(?MODULE,checkCredit,[15,Pid]),
-	  spawn(?MODULE,checkAddress,[Pid]),
-	  spawn(?MODULE,checkItem,[Pid]),
-	  receive
-	      true -> io:format("All checks successful.~n"),true;
-	      false -> io:format("Check failure.~n"),false
-	  end.
+-export([main/0, asynchAnd/2, checkCredit/2, checkAddress/1, checkItem/1]).
 
-asynchAnd(N,Out) ->
-    if 
-	N > 0 ->   receive 
-		       true -> asynchAnd(N-1,Out);
-		       false -> Out!false
-		   end;
-	true -> Out!true
-    end.
+main() ->
+  Pid = spawn(?MODULE, asynchAnd, [3, self()]),
+  spawn(?MODULE, checkCredit, [15, Pid]),
+  spawn(?MODULE, checkAddress, [Pid]),
+  spawn(?MODULE, checkItem, [Pid]),
+  receive
+    true -> io:format("All checks successful.~n"), true;
+    false -> io:format("Check failure.~n"), false
+  end.
 
-checkCredit(Price,Pid) -> if 
-			      Price < 10 -> io:format("Credit check successful~n"),Pid!true;
-			      true -> io:format("Credit check failed~n"),Pid!false
-			  end.
-					    
-checkAddress(Pid) -> io:format("Address check successful~n"),Pid!true.
+asynchAnd(N, Out) ->
+  if
+    N > 0 ->
+      receive
+        true -> asynchAnd(N - 1, Out);
+        false -> Out ! false
+      end;
+    true -> Out ! true
+  end.
 
-checkItem(Pid) -> io:format("Item check successful~n"),Pid!true.				   
-     
-     
-     
+checkCredit(Price, Pid) ->
+  if
+    Price < 10 -> io:format("Credit check successful~n"), Pid ! true;
+    true -> io:format("Credit check failed~n"), Pid ! false
+  end.
+
+checkAddress(Pid) -> io:format("Address check successful~n"), Pid ! true.
+
+checkItem(Pid) -> io:format("Item check successful~n"), Pid ! true.
+
+
+
