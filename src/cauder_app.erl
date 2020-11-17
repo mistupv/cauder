20154192b3823b16bbacb7fe8cd4ab482cd99c41
Juan José González Abril <juanjo@gonzabril.es>
2020-11-17 14:54:41 +0100

    Setup OTP Application files

---
+++ src/cauder_app.erl
@@ -0,0 +1,31 @@
+-module(cauder_app).
+
+-behaviour(application).
+
+%% application callbacks
+-export([start/2, stop/1]).
+
+
+%%%===================================================================
+%%% Application callbacks
+%%%===================================================================
+
+
+-spec start(StartType, StartArgs) -> {ok, Pid} | {ok, Pid, State} | {error, Reason} when
+  StartType :: application:start_type(),
+  StartArgs :: term(),
+  Pid :: pid(),
+  State :: term(),
+  Reason :: term().
+
+start(normal, []) ->
+  start(normal, [wx]);
+start(normal, Args) ->
+  cauder_sup:start_link(Args).
+
+
+-spec stop(State) -> ok when
+  State :: term().
+
+stop(_State) ->
+  ok.
