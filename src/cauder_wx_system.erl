8fa77021f4ef238bc3c0ae28951fab31e9c044c0
Juan José González Abril <juanjo@gonzabril.es>
2020-12-10 17:05:56 +0100

    Add ability to hide the messages panel, and show only the messages from the current process.

--- src/cauder_wx_system.erl
+++ src/cauder_wx_system.erl
@@ -21,7 +21,7 @@
   Window :: wxWindow:wxWindow().
 
 create(Parent) ->
-  Win = wxPanel:new(Parent),
+  Win = wxPanel:new(Parent, [{winid, ?SYSTEM_Panel}]),
 
   Sizer = wxStaticBoxSizer:new(?wxVERTICAL, Win, [{label, "System Info"}]),
   wxWindow:setSizer(Win, Sizer),
@@ -64,12 +64,12 @@ update(OldState, NewState) ->
   Window :: wxWindow:wxWindow().
 
 create_mail(Parent) ->
-  Win = wxPanel:new(Parent),
+  Win = wxPanel:new(Parent, [{winid, ?SYSTEM_Mail_Panel}]),
 
   Sizer = wxStaticBoxSizer:new(?wxHORIZONTAL, Win, [{label, "Mail"}]),
   wxPanel:setSizer(Win, Sizer),
 
-  MailArea = wxListCtrl:new(Win, [{winid, ?SYSTEM_Mail}, {style, ?wxLC_REPORT bor ?wxLC_SINGLE_SEL}]),
+  MailArea = wxListCtrl:new(Win, [{winid, ?SYSTEM_Mail_Control}, {style, ?wxLC_REPORT bor ?wxLC_SINGLE_SEL}]),
   wxBoxSizer:add(Sizer, MailArea, [{proportion, 1}, {flag, ?wxEXPAND}]),
 
   Item = wxListItem:new(),
@@ -102,14 +102,25 @@ create_mail(Parent) ->
   OldState :: cauder_wx:state(),
   NewState :: cauder_wx:state().
 
-update_mail(#wx_state{system = #sys{mail = Mail}}, #wx_state{system = #sys{mail = Mail}}) ->
+update_mail(
+    #wx_state{system = #sys{mail = Mail}, config = #config{mailbox = Show, mailbox_mode = all}},
+    #wx_state{system = #sys{mail = Mail}, config = #config{mailbox = Show, mailbox_mode = all}}) ->
+  ok;
+update_mail(
+    #wx_state{system = #sys{mail = Mail}, pid = Pid, config = #config{mailbox = Show, mailbox_mode = process}},
+    #wx_state{system = #sys{mail = Mail}, pid = Pid, config = #config{mailbox = Show, mailbox_mode = process}}) ->
+  ok;
+update_mail(_, #wx_state{config = #config{mailbox = false}}) ->
+  show_and_resize(cauder_wx:find(?SYSTEM_Mail_Panel, wxPanel), false),
   ok;
 update_mail(_, #wx_state{system = undefined}) ->
-  wxListCtrl:deleteAllItems(cauder_wx:find(?SYSTEM_Mail, wxListCtrl)),
+  show_and_resize(cauder_wx:find(?SYSTEM_Mail_Panel, wxPanel), true),
+  wxListCtrl:deleteAllItems(cauder_wx:find(?SYSTEM_Mail_Control, wxListCtrl)),
   ok;
-update_mail(_, #wx_state{system = #sys{mail = Mail}}) ->
+update_mail(_, #wx_state{system = #sys{mail = Mail}, config = #config{mailbox_mode = all}}) ->
+  show_and_resize(cauder_wx:find(?SYSTEM_Mail_Panel, wxPanel), true),
   Font = wxFont:new(9, ?wxTELETYPE, ?wxNORMAL, ?wxNORMAL),
-  MailArea = cauder_wx:find(?SYSTEM_Mail, wxListCtrl),
+  MailArea = cauder_wx:find(?SYSTEM_Mail_Control, wxListCtrl),
   wxListCtrl:freeze(MailArea),
   wxListCtrl:deleteAllItems(MailArea),
   lists:foldl(
@@ -121,6 +132,27 @@ update_mail(_, #wx_state{system = #sys{mail = Mail}}) ->
       wxListCtrl:setItem(MailArea, Row, 2, cauder_pp:to_string(Dest)),
       Row + 1
     end, 0, cauder_mailbox:to_list(Mail)),
+  wxListCtrl:thaw(MailArea),
+  ok;
+update_mail(_, #wx_state{system = #sys{mail = Mail}, pid = Pid, config = #config{mailbox_mode = process}}) ->
+  show_and_resize(cauder_wx:find(?SYSTEM_Mail_Panel, wxPanel), true),
+  Font = wxFont:new(9, ?wxTELETYPE, ?wxNORMAL, ?wxNORMAL),
+  MailArea = cauder_wx:find(?SYSTEM_Mail_Control, wxListCtrl),
+  wxListCtrl:freeze(MailArea),
+  wxListCtrl:deleteAllItems(MailArea),
+  case Pid of
+    undefined -> ok;
+    Pid ->
+      lists:foldl(
+        fun(#message{uid = Uid, value = Value, dest = Dest}, Row) ->
+          wxListCtrl:insertItem(MailArea, Row, ""),
+          wxListCtrl:setItemFont(MailArea, Row, Font),
+          wxListCtrl:setItem(MailArea, Row, 0, cauder_pp:to_string(Uid)),
+          wxListCtrl:setItem(MailArea, Row, 1, cauder_pp:to_string(Value)),
+          wxListCtrl:setItem(MailArea, Row, 2, cauder_pp:to_string(Dest)),
+          Row + 1
+        end, 0, cauder_mailbox:pid_get(Pid, Mail))
+  end,
   wxListCtrl:thaw(MailArea),
   ok.
 
@@ -210,4 +242,36 @@ update_roll_log(_, #wx_state{system = #sys{roll = RollLog}}) ->
   wxListBox:clear(RollLogArea),
   lists:foreach(fun(Entry) -> wxListBox:append(RollLogArea, Entry) end, RollLog),
   wxListBox:thaw(RollLogArea).
+
+
+%%%=============================================================================
+
+
+-spec show_and_resize(Panel, Show) -> ok when
+  Panel :: wxPanel:wxPanel(),
+  Show :: boolean().
+
+show_and_resize(Panel, Show) ->
+  case wxPanel:isShown(Panel) of
+    Show -> ok;
+    _ ->
+      wxPanel:show(Panel, [{show, Show}]),
+
+      % -----
+
+      SystemPanel = cauder_wx:find(?SYSTEM_Panel, wxPanel),
+      SystemSizer = wx:typeCast(wxPanel:getSizer(SystemPanel), wxSizer),
+
+      [Top, Spacer, Bottom] = wxSizer:getChildren(SystemSizer),
+
+      ShowTop = wxSizerItem:isShown(Top),
+      ShowBottom = wxSizerItem:isShown(Bottom),
+
+      wxSizerItem:show(Spacer, ShowTop and ShowBottom),
+
+      % -----
+
+      wxPanel:layout(SystemPanel),
+      ok
+  end.
 
