5b1f6664ebf6c88ba74f123e5766f1579c4362c4
Juan José González-Abril <juanjo@gonzabril.es>
2021-03-19 16:21:25 +0100

    Update repository URL

--- src/cauder.hrl
+++ src/cauder.hrl
@@ -3,7 +3,7 @@
 %%%=============================================================================
 
 -define(APP_NAME, "CauDEr").
--define(APP_URL, "https://github.com/mistupv/cauder-v2").
+-define(APP_URL, "https://github.com/mistupv/cauder").
 -define(APP_DB, 'cauder/database').
 
 
